19198b366c30aa0c736b3f74f3ca2db2f185355a
EC2 Default User <ec2-user@ip-172-31-41-121.eu-west-1.compute.internal>
2024-04-07 23:26:23 +0000

    views file pylint

--- Travel2gether/forms.py
+++ Travel2gether/forms.py
@@ -1,3 +1,5 @@
+"""forms file for app"""
+
 from django.contrib.auth.models import User
 from django import forms
 from .models import Profile, Post, Comment
@@ -6,20 +8,20 @@ from .models import Profile, Post, Comment
 # Form for registering new user
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
-    email = forms.EmailField(max_length=254, help_text='Required field')
+    email = forms.EmailField(max_length=254, help_text="Required field")
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ["username", "email", "password"]
 
 
 # Form for updating user email
 class UpdateUserForm(forms.ModelForm):
-    email = forms.EmailField(max_length=254, help_text='Required field')
+    email = forms.EmailField(max_length=254, help_text="Required field")
 
     class Meta:
         model = User
-        fields = ['email']
+        fields = ["email"]
 
 
 # Form for updating profile
@@ -27,20 +29,20 @@ class UpdateProfileForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = ['status_info', 'profile_photo']
+        fields = ["status_info", "profile_photo"]
 
 
 # Form for creating a post
 class CreatePost(forms.ModelForm):
-    
+
     class Meta:
         model = Post
-        fields = ['post_text', 'post_picture']
+        fields = ["post_text", "post_picture"]
 
 
 # Form for creating a comment
 class CreateComment(forms.ModelForm):
-    
+
     class Meta:
         model = Comment
-        fields = ['comment_text']
+        fields = ["comment_text"]
--- Travel2gether/views.py
+++ Travel2gether/views.py
@@ -1,16 +1,22 @@
 """ views.py file"""
 
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.generic import View
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
-from .forms import UserForm,UpdateUserForm,UpdateProfileForm,CreatePost,CreateComment
-from django.http import HttpResponse 
-from django.contrib.auth import authenticate,login
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import (
+    UserForm,
+    UpdateUserForm,
+    UpdateProfileForm,
+    CreatePost,
+    CreateComment,
+)
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from .models import User,Following,Follower,Post,Profile
+from .models import User, Following, Follower, Post, Profile
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.contrib import auth
@@ -18,266 +24,271 @@ from django.contrib.auth.hashers import make_password
 from django.shortcuts import get_object_or_404
 import itertools
 
-#Verifying input function
-def verifyInput(username,password):
-	error = []
-	if len(username) < 4:
-		error.append('Username Should At Least Be 4 Character Long')
-	elif len(password) < 8:
-		error.append('Password Should At Least Be 8 Character Long')
-	else:
-		print('OK')
 
-	return error
-	
+# Verifying input function
+def verifyInput(username, password):
+    error = []
+    if len(username) < 4:
+        error.append("Username Should At Least Be 4 Character Long")
+    elif len(password) < 8:
+        error.append("Password Should At Least Be 8 Character Long")
+    else:
+        print("OK")
 
-#Registering user
+    return error
+
+
+# Registering user
 def registerUser(request):
-	if request.method == 'POST':
-		username = request.POST['username']
-		email = request.POST['email']
-		password = request.POST['password']
-		hashed_password = make_password(password)
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        hashed_password = make_password(password)
 
-		error = verifyInput(username,password)
+        error = verifyInput(username, password)
 
-		if len(error) != 0:
-			error = error[0]
-			print(error)
-			return render(request, 'Travel2gether/registration_form.html', {'error': error})
-		else:
-			a = User(username=username, email=email, password=hashed_password)
-			a.save()
+        if len(error) != 0:
+            error = error[0]
+            print(error)
+            return render(
+                request, "Travel2gether/registration_form.html", {"error": error}
+            )
+        else:
+            a = User(username=username, email=email, password=hashed_password)
+            a.save()
 
-			user = User.objects.get(username=username)
-			user_id = User.objects.values_list('id', flat=True).filter( username = user)
-			print('User id',user_id)
-			Profile.objects.create(user_id=user_id)
-			messages.success(request, 'Account Was Created Successfully')
-			return redirect('register')
-	else:
-		return render(request, 'Travel2gether/registration_form.html')
+            user = User.objects.get(username=username)
+            user_id = User.objects.values_list("id", flat=True).filter(username=user)
+            print("User id", user_id)
+            Profile.objects.create(user_id=user_id)
+            messages.success(request, "Account Was Created Successfully")
+            return redirect("register")
+    else:
+        return render(request, "Travel2gether/registration_form.html")
 
 
-#defining index
+# defining index
 def index(request):
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(request, username=username, password=password)
-		if user is not None and user.is_active:
-			auth.login(request,user)
-			return redirect('welcome')
-		else:
-			messages.info(request, 'Invalid Username or Password')
-			return redirect('index')
-	else:
-		return render(request, 'Travel2gether/index.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return redirect("welcome")
+        else:
+            messages.info(request, "Invalid Username or Password")
+            return redirect("index")
+    else:
+        return render(request, "Travel2gether/index.html")
 
 
-#dashboard view
+# dashboard view
 def dashboard(request):
-	print(request.user)
-	return render(request, 'Travel2gether/dashboard.html')
+    print(request.user)
+    return render(request, "Travel2gether/dashboard.html")
 
-#feed function
+
+# feed function
 def feed(request):
-	try:
-		post_all = Post.objects.all().order_by('created_at')
-		print(post_all)
-	except Exception as e:
-		print(e)
+    try:
+        post_all = Post.objects.all().order_by("created_at")
+        print(post_all)
+    except Exception as e:
+        print(e)
 
-	comment_form = CreateComment()
-	username = request.user.username
+    comment_form = CreateComment()
+    username = request.user.username
 
-	context = {
-	'post_all': post_all,
-	'comment_form': comment_form,
-	'username': username,
-	}
+    context = {
+        "post_all": post_all,
+        "comment_form": comment_form,
+        "username": username,
+    }
 
-	return render(request, 'Travel2gether/feed.html', context)
+    return render(request, "Travel2gether/feed.html", context)
 
 
-#following functionality
+# following functionality
 def followweb(request, username):
-	if request.user.username != username:
-		if request.method == 'POST':
-			disciple = User.objects.get(username=request.user.username)
-			leader = User.objects.get(username=username)
-			
-			leader.follower_set.create(follower_user = disciple)
-			disciple.following_set.create(following_user = leader)
-			url = reverse('profile', kwargs = {'username' : username})
-			return redirect(url)
+    if request.user.username != username:
+        if request.method == "POST":
+            disciple = User.objects.get(username=request.user.username)
+            leader = User.objects.get(username=username)
+
+            leader.follower_set.create(follower_user=disciple)
+            disciple.following_set.create(following_user=leader)
+            url = reverse("profile", kwargs={"username": username})
+            return redirect(url)
 
 
-
-#unflowwing functionality
+# unflowwing functionality
 def unfollowweb(request, username):
-	if request.method == 'POST':
-		disciple = User.objects.get(username=request.user.username)
-		leader = User.objects.get(username=username)
-		
-		leader.follower_set.get(follower_user = disciple).delete()
-		disciple.following_set.get(following_user = leader).delete()
-		url = reverse('profile', kwargs = {'username' : username})
-		return redirect(url)
+    if request.method == "POST":
+        disciple = User.objects.get(username=request.user.username)
+        leader = User.objects.get(username=username)
 
-#posting on the profile
+        leader.follower_set.get(follower_user=disciple).delete()
+        disciple.following_set.get(following_user=leader).delete()
+        url = reverse("profile", kwargs={"username": username})
+        return redirect(url)
+
+
+# posting on the profile
 def postweb(request, username):
-	if request.method == 'POST':
-		post_form = CreatePost(request.POST, request.FILES)
-		if post_form.is_valid():
-			post_text = post_form.cleaned_data['post_text']
-			post_picture = post_form.cleaned_data['post_picture']
-			request.user.post_set.create(post_text=post_text, post_picture=post_picture)
-			messages.success(request, f'Post Was Created Successfully')
+    if request.method == "POST":
+        post_form = CreatePost(request.POST, request.FILES)
+        if post_form.is_valid():
+            post_text = post_form.cleaned_data["post_text"]
+            post_picture = post_form.cleaned_data["post_picture"]
+            request.user.post_set.create(post_text=post_text, post_picture=post_picture)
+            messages.success(request, f"Post Was Created Successfully")
 
-	url = reverse('profile', kwargs={'username':username})
-	return redirect(url)
+    url = reverse("profile", kwargs={"username": username})
+    return redirect(url)
 
 
-#commenting on the post
+# commenting on the post
 def commentweb(request, username, post_id):
-	if request.method == 'POST':
-		comment_form = CreateComment(request.POST)
+    if request.method == "POST":
+        comment_form = CreateComment(request.POST)
 
-		if comment_form.is_valid():
-			comment_text = comment_form.cleaned_data['comment_text']
+        if comment_form.is_valid():
+            comment_text = comment_form.cleaned_data["comment_text"]
 
-			user =User.objects.get(username=username)
-			post = user.post_set.get(pk=post_id)
-			post.comment_set.create(user=request.user, comment_text=comment_text)
-			messages.success(request, f'Comment Was Created Successfully')
+            user = User.objects.get(username=username)
+            post = user.post_set.get(pk=post_id)
+            post.comment_set.create(user=request.user, comment_text=comment_text)
+            messages.success(request, f"Comment Was Created Successfully")
 
-	url = reverse('profile', kwargs={'username':username})
-	return redirect(url)
+    url = reverse("profile", kwargs={"username": username})
+    return redirect(url)
 
 
-#search functionality
+# search functionality
 def search(request):
-    template='Travel2gether/search.html'
+    template = "Travel2gether/search.html"
 
-    query = request.GET['q']
+    query = request.GET["q"]
     print(query)
     data = query
 
     count = {}
     results = {}
-    results['posts']= User.objects.none()
+    results["posts"] = User.objects.none()
     queries = data.split()
     for query in queries:
-        results['posts'] = results['posts'] | User.objects.filter(username__icontains=query)
-        count['posts'] = results['posts'].count()
-
+        results["posts"] = results["posts"] | User.objects.filter(
+            username__icontains=query
+        )
+        count["posts"] = results["posts"].count()
 
     count2 = {}
     queries2 = data.split()
     results2 = {}
-    results2['posts'] = User.objects.none()
+    results2["posts"] = User.objects.none()
     queries2 = data.split()
     for query2 in queries:
-        results2['posts'] = results2['posts'] | User.objects.filter(first_name__icontains=query2)
-        count2['posts'] = results2['posts'].count()
-
+        results2["posts"] = results2["posts"] | User.objects.filter(
+            first_name__icontains=query2
+        )
+        count2["posts"] = results2["posts"].count()
 
     count3 = {}
     queries3 = data.split()
     results3 = {}
-    results3['posts'] = User.objects.none()
+    results3["posts"] = User.objects.none()
     queries3 = data.split()
     for query3 in queries:
-        results3['posts'] = results3['posts'] | User.objects.filter(last_name__icontains=query3)
-        count3['posts'] = results3['posts'].count()
-        
+        results3["posts"] = results3["posts"] | User.objects.filter(
+            last_name__icontains=query3
+        )
+        count3["posts"] = results3["posts"].count()
 
-    files = itertools.chain(results['posts'],results2['posts'], results3['posts'])
+    files = itertools.chain(results["posts"], results2["posts"], results3["posts"])
     result = []
     for i in files:
         if i not in result:
-            result.append(i)    
+            result.append(i)
 
-    paginate_by=2
+    paginate_by = 2
     username = request.user.username
-    print('current user',username)
-    person = User.objects.get(username = username)
-    print('person',person)
-	
-    context={ 'files':result }
-    return render(request,template,context)
+    print("current user", username)
+    person = User.objects.get(username=username)
+    print("person", person)
+
+    context = {"files": result}
+    return render(request, template, context)
 
 
-#profile function
+# profile function
 @login_required
 def profile(request, username):
-	
-	if request.method == 'POST':
-		u_form = UpdateUserForm(request.POST,instance=request.user)
-		p_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
-		
 
-		if u_form.is_valid() and p_form.is_valid():
+    if request.method == "POST":
+        u_form = UpdateUserForm(request.POST, instance=request.user)
+        p_form = UpdateProfileForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
 
-			u_form.save()
-			p_form.save()
-			
+        if u_form.is_valid() and p_form.is_valid():
 
-			messages.success(request,f'Your Profile has been updated!')
-			url = reverse('profile', kwargs = {'username' : username})
-			return redirect(url)
+            u_form.save()
+            p_form.save()
 
-	else:
-		if username == request.user.username:
-			u_form = UpdateUserForm(instance=request.user)
-			p_form = UpdateProfileForm(instance=request.user.profile)
-			post_form = CreatePost()
-			person = User.objects.get(username = username)
+            messages.success(request, f"Your Profile has been updated!")
+            url = reverse("profile", kwargs={"username": username})
+            return redirect(url)
 
-			context = {
-					'u_form':u_form,
-					'p_form':p_form,
-					'post_form':post_form,
-					'person':person,
-					
-			}	
-		else:
-			person = User.objects.get(username = username)
-			already_a_follower=0
-			for followers in person.follower_set.all():
-				if (followers.follower_user ==  request.user.username):
-					already_a_follower=1
-					break;
+    else:
+        if username == request.user.username:
+            u_form = UpdateUserForm(instance=request.user)
+            p_form = UpdateProfileForm(instance=request.user.profile)
+            post_form = CreatePost()
+            person = User.objects.get(username=username)
 
-			if already_a_follower==1:
-				context = {
-						'person':person,
-						
-							
-					}
-			else:
-				context = {
-						'person':person,
-						'f':1,
-						
-					}
-		comment_form = CreateComment()
-		context.update({'comment_form':comment_form})
+            context = {
+                "u_form": u_form,
+                "p_form": p_form,
+                "post_form": post_form,
+                "person": person,
+            }
+        else:
+            person = User.objects.get(username=username)
+            already_a_follower = 0
+            for followers in person.follower_set.all():
+                if followers.follower_user == request.user.username:
+                    already_a_follower = 1
+                    break
 
-	return render(request, 'Travel2gether/profile.html', context)
-	
-#deketing the post	
+            if already_a_follower == 1:
+                context = {
+                    "person": person,
+                }
+            else:
+                context = {
+                    "person": person,
+                    "f": 1,
+                }
+        comment_form = CreateComment()
+        context.update({"comment_form": comment_form})
+
+    return render(request, "Travel2gether/profile.html", context)
+
+
+# deketing the post
 def delete_post(request, username, post_id):
     post = get_object_or_404(Post, pk=post_id, user__username=username)
-    if request.method == 'POST':
+    if request.method == "POST":
         post.delete()
-        messages.success(request, f'Post Deleted Successfully')
-        return redirect('profile', username=username)
+        messages.success(request, f"Post Deleted Successfully")
+        return redirect("profile", username=username)
     # Handle GET request if necessary
-    return redirect('profile', username=username)
+    return redirect("profile", username=username)
 
-#default page
+
+# default page
 def welcome(request):
-	url = reverse('profile', kwargs = {'username' : request.user.username})
-	return redirect(url)
+    url = reverse("profile", kwargs={"username": request.user.username})
+    return redirect(url)
