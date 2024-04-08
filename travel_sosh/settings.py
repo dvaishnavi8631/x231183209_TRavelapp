6610cef4b3d8a4021e73bce2ed5d006e042d7d2a
EC2 Default User <ec2-user@ip-172-31-41-121.eu-west-1.compute.internal>
2024-04-08 01:02:24 +0000

    adding pylint

--- travel_sosh/settings.py
+++ travel_sosh/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 #LOGIN_REDIRECT_URL = 'welcome'
 #LOGIN_URL = 'login'
